ba00c2f6bbae0e47003cb72dd94e31474fec25c7
WeiShiming <shiming.wei@finture.id>
2025-03-30 17:51:17 +0800

    feat(wsm): modify skipFirstFrameCount

--- ios/Classes/MobileScanner.swift
+++ ios/Classes/MobileScanner.swift
@@ -16,7 +16,7 @@ typealias TorchModeChangeCallback = ((Int?) -> ())
 typealias ZoomScaleChangeCallback = ((Double?) -> ())
 
 public class MobileScanner: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate, FlutterTexture {
-    static let MAX_SKIP_FRAME_COUNT = 36
+    static let MAX_SKIP_FRAME_COUNT = 12
   
     /// Capture session of the camera
     var captureSession: AVCaptureSession?
