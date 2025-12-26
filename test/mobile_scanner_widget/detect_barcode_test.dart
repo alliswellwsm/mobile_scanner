28fdb154a416815d00cf41afef16a4433901f0eb
Navaron Bracke <brackenavaron@gmail.com>
2025-12-26 15:42:39 +0100

    chore - remove mocktail

--- test/mobile_scanner_widget/detect_barcode_test.dart
+++ test/mobile_scanner_widget/detect_barcode_test.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
-// TODO: add test for when controller is provided to widget
+// TODO: add test for when controller is provided to widget (detect barcodes / dispose controller)
+// TODO: add new test file to test for existence of error widget, if start fails
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
